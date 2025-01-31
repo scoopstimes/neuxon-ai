5bde48c7fb4d92b8777156d6fcf0586c911f0967
Januar <adhinugroho623@gmail.com>
2025-01-31 18:51:05 +0700

    update main.js

--- main.js
+++ main.js
@@ -148,7 +148,7 @@ const handleFormSubmit = (e) => {
         <div class="bot-message" style="display: flex; gap: 5px; margin-left: 0px;">
           <img src="neuxon.png" class="avatar">
           <div class="message-content">
-            <p class="message-text"></p>
+            <p class="message-text">Sedang Menjawab...</p>
             <div class="bot-controls">
               <button class="copy-btn" title="Copy"><span class="material-symbols-rounded">content_copy</span></button>
               <button class="like-btn" title="Like"><span class="material-symbols-rounded">thumb_up</span></button>
@@ -174,7 +174,7 @@ const handleFormSubmit = (e) => {
         <div class="bot-message" style="display: flex; gap: 5px; margin-left: 0px;">
           <img src="neuxon.png" class="avatar">
           <div class="message-content">
-            <p class="message-text"></p>
+            <p class="message-text">Sedang Menjawab...</p>
             <div class="bot-controls">
               <button class="copy-btn" title="Copy"><span class="material-symbols-rounded">content_copy</span></button>
               <button class="like-btn" title="Like"><span class="material-symbols-rounded">thumb_up</span></button>
